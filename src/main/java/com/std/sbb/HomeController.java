271929fb64c7dda0abc68a4a4558812e6c534c12
ckh <gusrudchl12@gmail.com>
2023-11-13 12:18:01 +0900

    11-13-springboot

--- src/main/java/com/std/sbb/HomeController.java
+++ src/main/java/com/std/sbb/HomeController.java
@@ -2,6 +2,7 @@ package com.std.sbb;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 //
@@ -9,22 +10,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 // 아래 있는 HomeController 는 컨트롤러이다.
 @Controller
 public class HomeController {
+    int num = 0;
     // 개발자가 스트링부트한테 말한다.
     // 만약에 /home/main 이런 요청이 오면 아래 메서드를 실행해줘.
     // @GetMapping("/home/main") 의 의미
     @ResponseBody
-    @GetMapping("/home/main")
-    public String ShowMain(){
-        return "안녕하세요.";
-    }
-    @ResponseBody
-    @GetMapping("/home/main2")
-    public String ShowMain2(){
-        return "반갑습니다.";
-    }
-    @ResponseBody
-    @GetMapping("/home/main3")
-    public String ShowMain3(){
-        return "즐거웠습니다.";
+    @GetMapping("/home/plus")
+    public int ShowMain(@RequestParam(defaultValue = "0") int a ,@RequestParam(defaultValue = "0") int b ){
+        return a+b;
     }
 }
