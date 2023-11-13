6bd537ceeb817193ce2c1e08afe7817968d8b769
ckhckh2 <gusrudchl12@gmail.com>
2023-11-13 18:53:30 +0900

    11-13-springboot

--- src/main/java/com/std/sbb/HomeController.java
+++ src/main/java/com/std/sbb/HomeController.java
@@ -1,68 +1,281 @@
 package com.std.sbb;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
-
-//
-// 개발자가 스트링부트한테 말한다.
+import java.util.Map;
+// @Controller 의 의미
+// 개발자가 스프링부트에게 말한다.
 // 아래 있는 HomeController 는 컨트롤러이다.
 @Controller
 public class HomeController {
+    private int number;
     private List<Person> people;
-
+    private int id;
     HomeController() {
+        id = 0;
+        number = -1;
         people = new ArrayList<>();
     }
 
-    // 개발자가 스트링부트한테 말한다.
-    // 만약에 /home/main 이런 요청이 오면 아래 메서드를 실행해줘.
     // @GetMapping("/home/main") 의 의미
-    //@RequestParam는 생략가능
-   @GetMapping("/home/addPerson")
-   @ResponseBody
-    public String retunrPerson(@RequestParam(defaultValue = "0") int age,@RequestParam(defaultValue = "null") String name) {
-       Person person = new Person(name, age);
+    // 개발자가 스프링부트에게 말한다.
+    // 만약에 /home/main 이런 요청이 오면 아래 메서드를 실행해줘
+    @GetMapping("/home/main")
+    @ResponseBody
+    public String ShowMain() {
+        return "안녕하세요";
+    }
+    //    http://localhost:8080/home/main2
+    //    응답 : 반갑습니다.
+    @GetMapping("/home/main2")
+    @ResponseBody
+    public String showMain2() {
+        return "반갑습니다.";
+    }
+    //    http://localhost:8080/home/main3
+    //    즐거웠습니다.
+    @GetMapping("/home/main3")
+    @ResponseBody
+    public String showMain3() {
+        return "즐거웠습니다.";
+    }
+    @GetMapping("/home/increase")
+    @ResponseBody
+    public int showIncrease() {
+        number++;
+        return number;
+    }
+    // @RequestParam 의 의미
+    // 개발자가 스프링부트에게 말한다.
+    // int a 는 쿼리스트링에서 a 파라미터의 값을 얻은 후 정수화 한 값이어야 한다.
+    // @RequestParam 는 생략가능
+    @GetMapping("/home/plus")
+    @ResponseBody
+    public int showPlus(@RequestParam(defaultValue = "0") int a, @RequestParam int b) {
+        return a + b;
+    }
 
+    @GetMapping("/home/minus")
+    @ResponseBody
+    public int showMinus(int a, int b) {
+        return a - b;
+    }
 
-       person.getId();
-       person.getAge();
-       person.getName();
+    @GetMapping("/home/multiply")
+    @ResponseBody
+    public int showMultiply(int a, int b) {
+        return a * b;
+    }
 
-       people.add(person);
+    @GetMapping("/home/divide")
+    @ResponseBody
+    public int showDivide(int a, int b) {
+        return a / b;
+    }
 
-       return person.getId() + "번 사람이 추가되었습니다.";
+    //
+    @GetMapping("/home/returnBoolean")
+    @ResponseBody
+    public boolean returnBoolean() {
+        return true;
+    }
 
-   }
+    @GetMapping("/home/returnDouble")
+    @ResponseBody
+    public double returnDouble() {
+        return Math.PI;
+    }
+
+    @GetMapping("/home/returnIntArray")
+    @ResponseBody
+    public int[] showReturnIntArray() {
+        int[] arr = new int[]{10, 20, 30};
+        return arr;
+    }
+    @GetMapping("/home/returnIntList")
+    @ResponseBody
+    public List<Integer> showReturnIntList() {
+        List<Integer> list = new ArrayList<>() {{
+            add(10);
+            add(20);
+            add(30);
+        }};
+        return list;
+    }
+    @GetMapping("/home/returnMap")
+    @ResponseBody
+    public Map<String, Object> showReturnMap() {
+        Map<String, Object> map = new LinkedHashMap<>() {{
+            put("id", 1);
+            put("speed", 100);
+            put("name", "카니발");
+            put("relatedIds", new ArrayList<>() {{
+                add(2);
+                add(3);
+                add(4);
+            }});
+        }};
+        return map;
+    }
+    @GetMapping("/home/returnCarV2")
+    @ResponseBody
+    public CarV2 showReturnCarV2() {
+        CarV2 car = new CarV2(1, 100, "카니발", new ArrayList<>() {{
+            add(2);
+            add(3);
+            add(4);
+        }});
+        car.setName(car.getName() + "V2");
+        return car;
+    }
+    @GetMapping("/home/returnCarMapList")
+    @ResponseBody
+    public List<Map<String, Object>> showReturnCarMapList() {
+        Map<String, Object> carMap1 = new LinkedHashMap<>() {{
+            put("id", 1);
+            put("speed", 100);
+            put("name", "카니발");
+            put("relatedIds", new ArrayList<>() {{
+                add(2);
+                add(3);
+                add(4);
+            }});
+        }};
+        Map<String, Object> carMap2 = new LinkedHashMap<>() {{
+            put("id", 2);
+            put("speed", 200);
+            put("name", "포르쉐");
+            put("relatedIds", new ArrayList<>() {{
+                add(5);
+                add(6);
+                add(7);
+            }});
+        }};
+        List<Map<String, Object>> list = new ArrayList<>();
+        list.add(carMap1);
+        list.add(carMap2);
+        return list;
+    }
+    @GetMapping("/home/returnCarList")
+    @ResponseBody
+    public List<CarV2> showReturnCarList() {
+        CarV2 car1 = new CarV2(1, 100, "카니발", new ArrayList<>() {{
+            add(2);
+            add(3);
+            add(4);
+        }});
+        CarV2 car2 = new CarV2(2, 200, "포르쉐", new ArrayList<>() {{
+            add(5);
+            add(6);
+            add(7);
+        }});
+        List<CarV2> list = new ArrayList<>();
+        list.add(car1);
+        list.add(car2);
+
+        return list;
+    }
+
+    @GetMapping("/home/addPerson")
+    @ResponseBody
+    public String addPerson (String name, int age) {
+        Person p  =  new Person(name, age);
+
+        System.out.println(p);
+        people.add(p);
+
+        return  p.getId() + "번 사람이 추가되었습니다.";
+//        return "%d번 사람이 추가되었습니다.".formatted(p.getId());
+    }
+    @GetMapping("/home/removePerson")
+    @ResponseBody
+    public String removePerson(@RequestParam(defaultValue = "0") int id){
+        Person person = findById(id);
+        people.remove(person);
+        return person.getId() + "번 사람이 삭제되었습니다.";
+    }
+    @GetMapping("/home/modifyPerson")
+    @ResponseBody
+    public String modifyPerson(@RequestParam(defaultValue = "0") int id, @RequestParam(defaultValue = "0") int age, @RequestParam(defaultValue = "null") String name){
+        Person person = findById(id);
+        person.setName(name);
+        person.setAge(age);
+        return person.getId() + "번 사람이 수정되었습니다.";
+    }
     @GetMapping("/home/people")
     @ResponseBody
-    public List<Person> showPeople(){
-
+    private List<Person> showPeople () {
         return people;
     }
+    public Person findById(int id){
+        for (Person people : showPeople()){
+            if (people.getId() == id){
+                return people;
+            }
+        }
+        return null;
+    }
 }
-@Getter
-@Setter
 @AllArgsConstructor
-class Person{
+@Getter
+@ToString
+class Person {
     private static int lastId;
-    private final int id;
-    private final String name;
-    private final int age;
+    private int id;
+    @Setter
+    private String name;
+    @Setter
+    private int age;
 
     static {
         lastId = 0;
     }
-    Person(String name, int age){
-        this.id  = lastId++;
-        this.age = age;
-        this.name = name;
+    Person (String name, int age) {
+//         this.id = lastId++;
+//         this.name = name;
+//         this.age = age;
+
+        this(++lastId, name, age);
     }
+}
+class Car {
+    private final int id;
+    private final int speed;
+    private final String name;
+    private final List<Integer> relatedIds;
+    public Car(int id, int speed, String name, List<Integer> relatedIds) {
+        this.id = id;
+        this.speed = speed;
+        this.name = name;
+        this.relatedIds = relatedIds;
+    }
+    public int getId() {
+        return id;
+    }
+    public int getSpeed() {
+        return speed;
+    }
+    public String getName() {
+        return name;
+    }
+    public List<Integer> getRelatedIds() {
+        return relatedIds;
+    }
+}
+@Getter
+@AllArgsConstructor
+class CarV2 {
+    private final int id;
+    private final int speed;
+    @Setter
+    private String name;
+    private final List<Integer> relatedIds;
 }
